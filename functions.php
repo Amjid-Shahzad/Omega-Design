241ec776a5f67ea9edfcb11cc14dcb7a675bd10a
Amjid-Shahzad <amjadshahzad.pk360@gmail.com>
2026-06-01 08:17:45 +0300

    Theme Initialization

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Twenty Twenty-Five functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/
+ *
+ * @package WordPress
+ * @subpackage omega-design
+ * @since omega-design 1.0
+ */
